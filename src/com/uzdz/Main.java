d4dda87d65dc84510ed33a0ad4bec815f19255f9
Zhen Dong <devmen@163.com>
2020-03-26 14:00:22 +0800

    first init

---
+++ src/com/uzdz/Main.java
@@ -0,0 +1,14 @@
+package com.uzdz;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>(16);
+
+        map.put("name", "uzdz");
+        map.put("job", "java");
+    }
+}
